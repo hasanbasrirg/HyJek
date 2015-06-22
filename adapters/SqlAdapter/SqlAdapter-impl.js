c1fee2ca51c50f3bcbbe8f213efb03fe5ed4ca82
Sirius Dely <siriusdely@icloud.com>
2015-06-23 00:04:05 +0700

    Trying to connect to mysql db

--- adapters/SqlAdapter/SqlAdapter-impl.js
+++ adapters/SqlAdapter/SqlAdapter-impl.js
@@ -5,13 +5,40 @@
  *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
+//Create SQL query
+var getAccountsTransactionsStatement = WL.Server.createSQLStatement( 
+  "SELECT transactionId, fromAccount, toAccount, transactionDate, transactionAmount, transactionType " + 
+  "FROM accounttransactions " + 
+  "WHERE accounttransactions.fromAccount = ? OR accounttransactions.toAccount = ? " +
+  "ORDER BY transactionDate DESC " + 
+  "LIMIT 20;"
+);
+
+//Invoke prepared SQL query and return invocation result  
+function getAccountTransactions1(accountId){
+  return WL.Server.invokeSQLStatement({
+    preparedStatement : getAccountsTransactionsStatement,
+    parameters : [accountId, accountId]
+  });
+}
+
+
+//Invoke stored SQL procedure and return invocation result
+function getAccountTransactions2(accountId){
+  return WL.Server.invokeSQLStoredProcedure({
+    procedure : "getAccountTransactions",
+    parameters : [accountId]
+    
+  });
+}
+
 /************************************************************************
  * Implementation code for procedure - 'procedure1'
  *
  *
  * @return - invocationResult
  */
- 
+/* 
 var procedure1Statement = WL.Server.createSQLStatement("select COLUMN1, COLUMN2 from TABLE1 where COLUMN3 = ?");
 function procedure1(param) {
 	return WL.Server.invokeSQLStatement({
@@ -19,18 +46,18 @@ function procedure1(param) {
 		parameters : [param]
 	});
 }
-
+*/
 /************************************************************************
  * Implementation code for procedure - 'procedure2'
  *
  *
  * @return - invocationResult
  */
- 
+/* 
 function procedure2(param) {
 	return WL.Server.invokeSQLStoredProcedure({
 		procedure : "storedProcedure2",
 		parameters : [param]
 	});
 }
-
+*/
