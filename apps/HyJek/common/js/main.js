c6621cd6cd0f316c5541faccd1624c7bc4d951de
Sirius Dely <siriusdely@icloud.com>
2015-06-23 20:50:11 +0700

    Finish SingleStepAuthenticationAdapter sample

--- apps/HyJek/common/js/main.js
+++ apps/HyJek/common/js/main.js
@@ -1,65 +1,19 @@
-var busyIndicator = null;
-var citiesList = null;
-
 function wlCommonInit(){
-  busyIndicator = new WL.BusyIndicator("AppBody");
-  $('#citiesList').change(citySelectionChange);
-  getCitiesList();
+
 }
 
-function getCitiesList() {
-  busyIndicator.show();
-  var resourceRequest = new WLResourceRequest("/adapters/SqlWeather/getCitiesWeather", WLResourceRequest.GET, 30000);
+function getSecretData(){
+  var resourceRequest = new WLResourceRequest("/adapters/SingleStepAuthAdapter/getSecretData", WLResourceRequest.GET, 30000);
   resourceRequest.send().then(
-    getCitiesListSuccess,
-    getCitiesListFailure
-  );
-
-  /*
-  var invocationData = {
-      adapter:    'SqlWeather',
-      procedure:  'getCitiesWeather',
-  };
-  WL.Client.invokeProcedure(invocationData,{
-      onSuccess : getCitiesListSuccess,
-      onFailure : getCitiesListFailure
-  });
-  */
-}
-
-function getCitiesListSuccess(response) {
-  if (response.responseJSON.resultSet.length == 0)
-    getCitiesListFailure();
-  else {
-    citiesList = response.responseJSON.resultSet;
-    fillCitiesList();
-  }
-}
-
-function getCitiesListFailure(response) {
-  WL.Logger.debug("CityWeather::getCitiesListFailure");
-  busyIndicator.hide();
-  WL.SimpleDialog.show("CityWeather",
-    "Can't get cities list. Check database connection", [{
-      text : 'Reload app',
-      handler : WL.Client.reloadApp
-    }]
+    getSecretData_CallbackOK,
+    getSecretData_CallbackFail
   );
 }
 
-function fillCitiesList(){
-  $('#citiesList').empty();
-  for (var i = 0; i < citiesList.length; i++) {
-    var elem = $("<option/>").html(citiesList[i].city);
-    $('#citiesList').append(elem);
-  }
-  busyIndicator.hide();
-  citySelectionChange();
+function getSecretData_CallbackOK(response){
+  $("#ResponseDiv").html(JSON.stringify(response.responseJSON));
 }
 
-function citySelectionChange() {
-  var index = $('#citiesList').prop("selectedIndex");
-  var citySumm = citiesList[index].summary;
-  var cityWeather = citiesList[index].weather;
-  $('#info').html(cityWeather + "<br>" + citySumm.slice(0, 200) + "...");
+function getSecretData_CallbackFail(response){
+  $("#ResponseDiv").html(JSON.stringify(response.responseJSON));
 }
